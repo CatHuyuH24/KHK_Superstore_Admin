73044fff694357413f9f7312ac12fcbf893afef7
Manh Trong Kien <manhtrongkien1901@gmail.com>
2024-11-26 20:31:09 +0700

    nothing

--- apps/category/categoryController.js
+++ apps/category/categoryController.js
@@ -24,6 +24,7 @@ async function renderCategoryPage(req, res) {
    );
 
    const response = {
+      title: "Category Page",
        error: false,
        total: products.total,
        page: page + 1,
