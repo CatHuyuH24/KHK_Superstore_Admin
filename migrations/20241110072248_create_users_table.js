6e4fe0fbd8ed82b45ba862ee51b574a0399d4c91
Ma Cat Huynh <uynhhuc810@gmail.com>
2024-12-02 23:49:56 +0700

    feat: update db migrations script
    - Update db migrations script to create new tables, link tables and update existing tables' columns
    - Remove old seeding files

--- migrations/20241110072248_create_users_table.js
+++ migrations/20241110072248_create_users_table.js
@@ -3,17 +3,21 @@
  * @returns { Promise<void> }
  */
 exports.up = async function (knex) {
-	await knex.raw(
-		`CREATE TABLE IF NOT EXISTS users (
-    ID SERIAL PRIMARY KEY,
-    name VARCHAR(255) ,
+  await knex.raw(
+    `CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    real_name VARCHAR(255),
+    username VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    salt VARCHAR(255)
+    hashed_password VARCHAR(255) NOT NULL,
+    salt VARCHAR(255) NOT NULL,
+    role VARCHAR(32) NOT NULL,
+    avatar_img_url VARCHAR,
+    is_active BOOLEAN DEFAULT TRUE NOT NULL
 );`
-	);
+  );
 };
 
 exports.down = async function (knex) {
-	await knex.raw(`DROP TABLE IF EXISTS users;`);
+  await knex.raw(`DROP TABLE IF EXISTS users;`);
 };
---
+++ migrations/20241202155134_create_orders_table.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function (knex) {
+  await knex.raw(`
+    CREATE TABLE IF NOT EXISTS orders (
+      id SERIAL PRIMARY KEY,
+      user_id INT NOT NULL,
+      shipping_detail VARCHAR,
+      is_paid BOOLEAN NOT NULL DEFAULT FALSE,
+      order_date timestamp without time zone NOT NULL DEFAULT NOW(),
+      FOREIGN KEY (user_id) REFERENCES users(id)
+    );`);
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {};
