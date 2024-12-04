80cbe9f5bf434dd6286b63ca315cb6a5a6350bd8
Nguyễn Minh Khang <mkhang040506@gmail.com>
2024-12-04 20:36:09 +0700

    checkout moves items from cart to order

--- apps/checkout/checkoutRouter.js
+++ apps/checkout/checkoutRouter.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const checkoutController = require("./checkoutController");
+const utils = require("../Utils/jwtUtils")
 
 // Route to render the checkout page
-router.get('/', (req, res) => {
-    res.render('checkout');
-});
+router.get('/',utils.authMiddleware({ session: true }) ,checkoutController.renderCheckoutPage)
 
 module.exports = router;
