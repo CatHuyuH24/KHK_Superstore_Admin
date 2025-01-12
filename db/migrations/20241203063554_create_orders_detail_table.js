9497a945ee8a52aab573166a4e440d2cad6cbaed
Manh Trong Kien <manhtrongkien1901@gmail.com>
2025-01-12 15:15:48 +0700

    update order table and order_detail table, update icon when login

--- db/migrations/20241203063554_create_orders_detail_table.js
+++ db/migrations/20241203063554_create_orders_detail_table.js
@@ -9,10 +9,32 @@ exports.up = async function (knex) {
         product_id INT NOT NULL,
         quantity INT NOT NULL,
         price INT NOT NULL,
-        FOREIGN KEY (order_id) REFERENCES orders(id) ON DELETE CASCADE,
+        FOREIGN KEY (order_id) REFERENCES orders(order_id) ON DELETE CASCADE,
         FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE,
         PRIMARY KEY (order_id, product_id)
-        );`);
+    );`);
+
+    await knex.raw(`
+      CREATE SEQUENCE IF NOT EXISTS order_code_seq  START 1;
+    `);
+  
+    await knex.raw(`
+      CREATE OR REPLACE FUNCTION generate_order_code()
+      RETURNS TRIGGER AS $$
+      BEGIN
+          NEW.order_code := 'DH' || LPAD(nextval('order_code_seq')::TEXT, 3, '0');
+          RETURN NEW;
+      END;
+      $$ LANGUAGE plpgsql;
+    `);
+  
+    await knex.raw(`
+      CREATE TRIGGER trg_generate_order_code
+      BEFORE INSERT ON orders
+      FOR EACH ROW
+      EXECUTE FUNCTION generate_order_code();
+    `);
+
   };
   
   /**
@@ -21,5 +43,9 @@ exports.up = async function (knex) {
    */
   exports.down = async function (knex) {
     await knex.raw(`DROP TABLE IF EXISTS orders_detail;`);
+    await knex.raw(`DROP TRIGGER IF EXISTS trg_generate_order_code ON orders`);
+    await knex.raw(`DROP FUNCTION IF EXISTS generate_order_code`);
+  
+    await knex.raw(`DROP SEQUENCE IF EXISTS order_code_seq`);
   };
   
