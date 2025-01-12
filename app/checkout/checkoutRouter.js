b800d04203ba55718ccef907a30081f2c6c60936
Manh Trong Kien <manhtrongkien1901@gmail.com>
2025-01-12 16:10:45 +0700

    checkout function

--- app/checkout/checkoutRouter.js
+++ app/checkout/checkoutRouter.js
@@ -5,5 +5,6 @@ const utils = require("../Utils/jwtUtils")
 
 // Route to render the checkout page
 router.get('/',utils.authMiddleware({ session: true }) ,checkoutController.renderCheckoutPage)
-
+router.post('/create',checkoutController.SaveOrderToDB);
+router.get('/payment',checkoutController.renderPaymentPage);
 module.exports = router;
--- app/checkout/checkoutService.js
+++ app/checkout/checkoutService.js
@@ -25,6 +25,153 @@ const getProductInCartByUserIdToOrder= async(user_id)=>{
     }
 }
 
+async function createNewOrder(user_id, total, address) {
+    try {
+        const result = await pool.query(
+            `INSERT INTO orders (user_id, total, address) 
+             VALUES ($1, $2, $3) RETURNING order_id, order_code`, 
+            [user_id, total, address]
+        );
+        
+        const order_id = result.rows[0].order_id;
+        const order_code=result.rows[0].order_code
+        return {order_id:order_id,order_code:order_code}; 
+       
+    } catch (error) {
+        console.error('Error creating new order by user_id', error);
+        throw error; 
+    }
+}
+
+async function createOrderDetail(order_id,product_id,quantity){
+    try{
+        const result=await pool.query(`insert into orders_detail (order_id,product_id,quantity) values($1,$2,$3)`,[order_id,product_id,quantity]);
+    }catch(error){
+        console.error('Error creating new order detail', error);
+        throw error; 
+    }
+} 
+
+async function findOrderWithDetails(order_code, user_id) {
+    try {
+      
+        const orderResult = await pool.query(`select order_id, total, status from orders where order_code=$1 and user_id=$2`, [order_code,user_id]);
+        
+
+        if (orderResult.rows.length === 0) {
+            return [];
+        }
+
+        const order = orderResult.rows[0]; 
+      
+        const detailsResult = await pool.query(`
+            Select p.image_url, p.name, d.quantity, p.price, p.price*p.discount/100 AS discount_price
+            from orders_detail d 
+            join products p on d.product_id=p.id
+            where d.order_id=$1`, [order.order_id]);
+        
+    
+        return {
+            order: order,
+            details: detailsResult.rows
+        };
+
+    } catch (error) {
+        console.error('Error fetching order and details',error);
+        return { order: [], details: [] };
+    }
+}
+
+async function updateOrderStatus(order_code, status) {
+    try {
+        const orderResult = await pool.query(
+            `SELECT order_id, status FROM orders WHERE order_code = $1`, 
+            [order_code]
+        );
+
+        if (orderResult.rows.length === 0) {
+            return { success: false, message: 'Can not find any order.' };
+        }
+
+        if(orderResult.rows[0].status==='Canceled'){
+            return {success:false,message: 'The order has been canceled'}
+        }
+
+        await pool.query(
+            `UPDATE orders 
+             SET status = $1, 
+                 ${status === 'Canceled' ? 'canceled_at = CURRENT_TIMESTAMP,' : ''} 
+             updated_at = CURRENT_TIMESTAMP
+             WHERE order_code = $2`,
+            [status, order_code]
+        );
+        
+        return {success:true,message:'Order has been canceled successfully'};
+    } catch (error) {
+        console.error('Error when update order status:', error);
+        return { success: false};
+    }
+}
+
+
+
+async function getAllOrderAndOrderItemByUserID(status, search, user_id) {
+    try {
+      const {
+        orderFilter, 
+        searchFilter, 
+      } = prepareFilterService.prepareFilterStatementsForOrder(status, search);
+  
+      const result = await pool.query(
+        `
+        SELECT 
+          order_id,
+          status AS order_status,
+          status_payment,
+          order_code,
+          total
+        FROM orders 
+        WHERE user_id=$1
+        ${orderFilter}
+        ${searchFilter}
+        `,
+        [user_id]
+      );
+  
+
+      const ordersWithProducts = [];
+  
+      for (const order of result.rows) {
+        order.products = [];
+
+        const detailsResult = await pool.query(`
+          SELECT p.image_url, p.name, d.quantity, p.price
+          FROM orders_detail d
+          JOIN products p ON d.product_id=p.id
+          WHERE d.order_id=$1
+        `, [order.order_id]);
+  
+        order.products = detailsResult.rows;
+  
+        ordersWithProducts.push(order);
+      }
+  
+      return ordersWithProducts;
+  
+    } catch (error) {
+      console.error(
+        `Error fetching orders and order items from orders:`,
+        error.message
+      );
+      return { orders: [] };
+    }
+  }  
+
 module.exports ={
-    getProductInCartByUserIdToOrder
+    getProductInCartByUserIdToOrder,
+    createOrderDetail,
+    createNewOrder,
+    findOrderWithDetails,
+    updateOrderStatus,
+    getAllOrderAndOrderItemByUserID
 }
